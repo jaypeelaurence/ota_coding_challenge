2b60aa8507ce5928c31a02c4f3934c2c4d565b19
Jaypee Laurence Cocjin <jaypeelaurencecocjin@gmail.com>
2024-01-19 05:31:38 +0800

    Update index.ts

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -2,8 +2,6 @@ const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const HEADERS = {
   ContentType: 'text/html',
 };
-const PER_PAGE = 10;
-const PAGE = 12;
 
 const DIR_PATH = `${process.cwd()}/src`
 
@@ -11,6 +9,4 @@ export default {
   DIR_PATH,
   DATE_FORMAT,
   HEADERS,
-  PER_PAGE,
-  PAGE,
 };
